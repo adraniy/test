603654a4dad363be4cced5bab618c2ae0460618f
Andrew Dranyy <AVDranyy.SBT@sberbank.ru>
2015-08-20 16:49:45 +0300

    add nane in A

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -3,6 +3,15 @@
  */
 public class A {
     int id;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
