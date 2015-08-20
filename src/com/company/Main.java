3ab7d227cc76ff849259eed864a8827d7dd2100a
Andrew Dranyy <AVDranyy.SBT@sberbank.ru>
2015-08-20 11:24:00 +0300

    task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         A a = new A();
+        C c = new C();
         System.out.println("A = " + a);
+        System.out.println("C = " + c);
     }
 }
