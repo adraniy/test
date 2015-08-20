b0df85e4a573358fe9627688fe661487e4beb829
Andrew Dranyy <AVDranyy.SBT@sberbank.ru>
2015-08-20 11:25:12 +0300

    main commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         A a = new A();
+        B b = new B();
         System.out.println("A = " + a);
+        System.out.println("B = " + b);
     }
 }
